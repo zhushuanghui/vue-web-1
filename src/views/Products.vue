3d16d947cc1b4abfcf62457d231ab60deb8835e9
zhushuanghui <1625149893@qq.com>
2020-04-10 11:39:06 +0800

    34

---
+++ src/views/Products.vue
@@ -0,0 +1,58 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-04-10 11:22:32
+ * @LastEditTime: 2020-04-10 11:27:05
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /tt/web/src/views/Messages.vue
+ -->
+
+<template>
+  <div class="products">
+    <div class="banner" :style="bg">
+      <!-- <img :src="banner" alt=""> -->
+    </div>
+    <section class="content">
+      news
+    </section>
+  </div>
+</template>
+<script>
+import contentslot from '@/components/Content.vue';
+
+export default {
+  name:'products',
+  data(){
+    return{
+      bg: {
+        backgroundImage: "url(" + require("../assets/images/about.jpg") + ")",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover"
+      }
+    }
+  },
+  components:{
+    contentslot
+  }
+}
+</script>
+<style lang="less" scope>
+  .banner{
+    width: 100%;
+    height: 380px;
+    // overflow: hidden;
+  }
+  .flex{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .content{
+    width: 70%;
+    margin: 0 auto;
+    padding: 40px 0;
+  }
+  .article{
+    flex: 1;
+  }
+</style>
